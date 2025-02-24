d4183e76c84409b466429ed9518aa096f0668b1e
Piyush-Joshi-1002 <piyushjoshi1002@gmail.com>
2025-02-24 09:03:16 -0500

    Fixed Repository Issue

--- src/main/java/com/learnkafka/entity/Book.java
+++ src/main/java/com/learnkafka/entity/Book.java
@@ -1,9 +1,6 @@
 package com.learnkafka.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,16 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
 public class Book {
 
     @Id
-    Integer bookId;
+    private Integer bookId;
 
-    String bookName;
-
-    String bookAuthor;
-
+    private String bookName;
+    private String bookAuthor;
 
     @OneToOne
     @JoinColumn(name = "libraryEventId")
